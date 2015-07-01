8ee7c67fffb525af2031699197ffa96fcd13fdf5
novohispano <novohispano@gmail.com>
2015-07-01 16:07:28 -0600

    Add Robots.txt

--- config.rb
+++ config.rb
@@ -28,6 +28,9 @@ begin
   # Renders Sitemap XML
   page "/sitemap.xml", layout: false
 
+  # Renders Robots Text
+  page "/robots.txt", layout: false
+
   # Proxy pages (http://middlemanapp.com/dynamic-pages/)
   # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
   #  :which_fake_page => "Rendering a fake page with a local variable" }
