fcdef4cdfd6dfb844b02f57a171d01d8f2f9b896
novohispano <novohispano@gmail.com>
2015-07-01 16:01:47 -0600

    Add sitemap

--- config.rb
+++ config.rb
@@ -25,6 +25,9 @@ begin
   #   page "/admin/*"
   # end
 
+  # Renders Sitemap XML
+  page "/sitemap.xml", layout: false
+
   # Proxy pages (http://middlemanapp.com/dynamic-pages/)
   # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
   #  :which_fake_page => "Rendering a fake page with a local variable" }
